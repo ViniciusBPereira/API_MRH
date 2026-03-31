e30dd581316db207f43224d8acd6777a691a14c7
Vinicius Begosso Pereira <37528824+ViniciusBPereira@users.noreply.github.com>
2026-03-31 17:38:09 -0300

    Update corpDb.js

--- src/config/corpDb.js
+++ src/config/corpDb.js
@@ -10,11 +10,14 @@ const corpPool = new Pool({
   password: env.CORP_DB_PASS,
   database: env.CORP_DB_NAME,
   ssl: false,
-  max: 5,
+
+  max: 3, // 🔥 reduz pressão na VPN
+
+  connectionTimeoutMillis: 60000, // 🔥 60s
+  statement_timeout: 120000,      // 🔥 2 min
+  query_timeout: 120000,
+
   idleTimeoutMillis: 30000,
-  connectionTimeoutMillis: 30000, // 30s
-  statement_timeout: 60000,       // 60s
-  query_timeout: 60000,
 });
 
 export default corpPool;
